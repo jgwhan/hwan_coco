d4d3a4141e486fd5f361f96b31bef596af19d29d
jaesik <jaesik@DESKTOP-R6LDGNR>
2016-12-28 01:33:50 +0900

    스터디모집 게시판 완성

---
+++ cocogit/WebContent/WEB-INF/views/studyBoard/created.jsp
@@ -0,0 +1,157 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%
+   String cp = request.getContextPath();
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>spring</title>
+
+<link href="<%=cp%>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<script src="<%=cp%>/jquery/jquery-3.1.1.min.js"></script>
+<script src="<%=cp%>/bootstrap/js/bootstrap.min.js"></script>
+<style type="text/css">
+.bs-write table {
+    width: 100%;
+    border: 0;
+    border-spacing: 0;
+}
+.table tbody tr td {
+    border-top: none;
+    font-weight: normal;
+	font-family: NanumGothic, 나눔고딕, "Malgun Gothic", "맑은 고딕", 돋움, sans-serif;
+} 
+.bs-write table td {
+	padding: 3px 3px 3px 3px;
+}
+
+.bs-write .td1 {
+    min-width: 100px;
+    min-height: 30px;
+    color: #666;
+    vertical-align: middle;
+}
+
+.bs-write .td2 {
+}
+
+.bs-write .td3 {
+}
+
+.bs-write .td4 {
+}
+</style>
+
+<script type="text/javascript" src="<%=cp%>/res/jquery/js/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+  function check() {
+        var f = document.boardForm;
+
+    	var str = f.subject.value;
+        if(!str) {
+            f.subject.focus();
+            return false;
+        }
+
+    	str = f.content.value;
+        if(!str) {
+            f.content.focus();
+            return false;
+        }
+
+    	var mode="${mode}";
+    	if(mode=="created")
+    		f.action="<%=cp%>/studyBoard/created_ok.do";
+    	else if(mode=="update")
+    		f.action="<%=cp%>/studyBoard/update_ok.do";
+
+    	// <input type='submit' ..>,  <input type='image' ..>, <button>은 submit() 메소드 호출하면 두번전송
+        return true;
+ }
+</script>
+
+</head>
+<body>
+
+<div>
+    <jsp:include page="/WEB-INF/layout/header.jsp"></jsp:include>
+</div>
+
+<div class="container" role="main">
+    <div class="bodyFrame col-sm-11"  style="float:none; margin-left: auto; margin-right: auto;">
+    
+	    <div class="body-title">
+	          <h3><span class="glyphicon glyphicon-book"></span> 스터디 모집 </h3>
+	    </div>
+	    
+	    <div class="alert alert-info" style="background-color: gray;color: black;border-color: activeborder;">
+	        <i class="glyphicon glyphicon-info-sign" style="black"></i> 프로그래밍 공부를 위한 스터디를 모집하세요.
+	    </div>
+	    
+	    <div>
+	        <form name="boardForm" method="post" onsubmit="return check();">
+	            <div class="bs-write">
+	                <table class="table">
+	                    <tbody>
+	                        <tr>
+	                            <td class="td1">작성자명</td>
+	                            <td class="td2 col-md-5 col-sm-5">
+	                                <p class="form-control-static">${sessionScope.member.userName}</p>
+	                            </td>
+	                            <td class="td1" align="center">&nbsp;</td>
+	                            <td class="td2 col-md-5 col-sm-5">
+	                                &nbsp;
+	                            </td>
+	                        </tr>
+	                        <tr>
+	                            <td class="td1">제목</td>
+	                            <td colspan="3" class="td3">
+	                                <input type="text" name="subject" class="form-control input-sm" value="${dto.subject}" required="required">
+	                            </td>
+	                        </tr>
+	                        <tr>
+	                            <td class="td1" colspan="4" style="padding-bottom: 0px;">내용</td>
+	                        </tr>
+	                        <tr>
+	                            <td colspan="4" class="td4">
+	                            	<textarea name="content" class="form-control" rows="13" required="required">${dto.content}</textarea>
+	                            </td>
+	                        </tr>
+	                    </tbody>
+	                    <tfoot>
+	                        <tr>
+	                            <td colspan="4" style="text-align: center; padding-top: 15px;">
+	                                  <button type="submit" class="btn btn-default"> 확인 <span class="glyphicon glyphicon-ok"></span></button>
+	                                  <button type="button" class="btn btn-default" onclick="javascript:location.href='<%=cp%>/studyBoard/list.do';"> 취소 </button>
+	                                  
+	                                  <c:if test="${mode=='update'}">
+	                                      <input type="hidden" name="num" value="${dto.num}">
+	                                      <input type="hidden" name="page" value="${page}">
+	                                  </c:if>
+	                            </td>
+	                        </tr>
+	                    </tfoot>
+	                </table>
+	            </div>
+	        </form>
+	    </div>
+    
+    </div>
+</div>
+
+<div>
+    <jsp:include page="/WEB-INF/layout/footer.jsp"></jsp:include>
+</div>
+
+<script type="text/javascript" src="<%=cp%>/res/jquery/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="<%=cp%>/res/jquery/js/jquery.ui.datepicker-ko.js"></script>
+<script type="text/javascript" src="<%=cp%>/res/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ cocogit/src/com/studyBoard/studyBoardServlet.java
@@ -0,0 +1,460 @@
+package com.studyBoard;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.member.SessionInfo;
+import com.util.MyServlet;
+import com.util.MyUtil;
+
+@WebServlet("/studyBoard/*")
+public class studyBoardServlet extends MyServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+
+		String uri = req.getRequestURI();
+
+		// uri에 따른 작업 구분
+		if (uri.indexOf("list.do") != -1) {
+			list(req, resp);
+		} else if (uri.indexOf("created.do") != -1) {
+			createdForm(req, resp);
+		} else if (uri.indexOf("created_ok.do") != -1) {
+			createdSubmit(req, resp);
+		} else if (uri.indexOf("article.do") != -1) {
+			article(req, resp);
+		} else if (uri.indexOf("update.do") != -1) {
+			updateForm(req, resp);
+		} else if (uri.indexOf("update_ok.do") != -1) {
+			updateSubmit(req, resp);
+		} else if (uri.indexOf("delete.do") != -1) {
+			delete(req, resp);
+		} else if (uri.indexOf("listReply.do") != -1) {
+			listReply(req, resp);
+		} else if (uri.indexOf("insertReply.do") != -1) {
+			insertReply(req, resp);
+		} else if (uri.indexOf("deleteReply.do") != -1) {
+			deleteReply(req, resp);
+		}
+	}
+
+	private void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 게시물 리스트
+		String cp = req.getContextPath();
+
+		studyBoardDAO dao = new studyBoardDAO();
+		MyUtil util = new MyUtil();
+		
+		String page = req.getParameter("page");
+		int current_page = 1;
+		if (page != null)
+			current_page = Integer.parseInt(page);
+
+		// 검색
+		String searchKey = req.getParameter("searchKey");
+		String searchValue = req.getParameter("searchValue");
+		if (searchKey == null) {
+			searchKey = "subject";
+			searchValue = "";
+		}
+		// GET 방식인 경우 디코딩
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			searchValue = URLDecoder.decode(searchValue, "utf-8");
+		}
+
+		// 전체 데이터 개수
+		int dataCount;
+		if (searchValue.length() == 0)
+			dataCount = dao.dataCount();
+		else
+			dataCount = dao.dataCount(searchKey, searchValue);
+
+		// 전체 페이지 수
+		int numPerPage = 10;
+		int total_page = util.pageCount(numPerPage, dataCount);
+
+		if (current_page > total_page)
+			current_page = total_page;
+
+		// 게시물 가져올 시작과 끝
+		int start = (current_page - 1) * numPerPage + 1;
+		int end = current_page * numPerPage;
+
+		// 게시물 가져오기
+		List<studyBoardDTO> list = null;
+		if (searchValue.length() == 0)
+			list = dao.listBoard(start, end);
+		else
+			list = dao.listBoard(start, end, searchKey, searchValue);
+
+		// 리스트 글번호 만들기
+		int listNum, n = 0;
+		Iterator<studyBoardDTO> it = list.iterator();
+		while (it.hasNext()) {
+			studyBoardDTO dto = it.next();
+			listNum = dataCount - (start + n - 1);
+			dto.setListNum(listNum);
+			n++;
+		}
+
+		String params = "";
+		if (searchValue.length() != 0) {
+			// 검색인 경우 검색값 인코딩
+			searchValue = URLEncoder.encode(searchValue, "utf-8");
+			params = "searchKey=" + searchKey + "&searchValue=" + searchValue;
+		}
+
+		// 페이징 처리
+		String listUrl = cp + "/studyBoard/list.do";
+		String articleUrl = cp + "/studyBoard/article.do?page=" + current_page;
+		if (params.length() != 0) {
+			listUrl += "?" + params;
+			articleUrl += "&" + params;
+		}
+
+		String paging = util.paging(current_page, total_page, listUrl);
+
+		// 포워딩할 JSP로 넘길 속성
+		req.setAttribute("list", list);
+		req.setAttribute("page", current_page);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("dataCount", dataCount);
+		req.setAttribute("paging", paging);
+		req.setAttribute("articleUrl", articleUrl);
+
+		// JSP로 포워딩
+		String path = "/WEB-INF/views/studyBoard/list.jsp";
+		forward(req, resp, path);
+	}
+	
+	private void createdForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글쓰기 폼
+		String cp=req.getContextPath();
+		
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		if (info == null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp + "/member/login.do");
+			return;
+		}
+
+		req.setAttribute("mode", "created");
+		String path = "/WEB-INF/views/studyBoard/created.jsp";
+		forward(req, resp, path);
+	}
+	
+	private void createdSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글 저장
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		if (info == null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp + "/member/login.do");
+			return;
+		}
+
+		studyBoardDTO dto = new studyBoardDTO();
+
+		// userId는 세션에 저장된 정보
+		dto.setUserId(info.getUserId());
+
+		// 파라미터
+		dto.setSubject(req.getParameter("subject"));
+		dto.setContent(req.getParameter("content"));
+
+		dao.insertBoard(dto);
+
+		resp.sendRedirect(cp + "/studyBoard/list.do");
+	}
+
+	private void article(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글보기
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		if (info == null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp + "/member/login.do");
+			return;
+		}
+
+		// 파라미터 : num, page, [searchKey, searchValue]
+		int num = Integer.parseInt(req.getParameter("num"));
+		String page = req.getParameter("page");
+		String searchKey = req.getParameter("searchKey");
+		String searchValue = req.getParameter("searchValue");
+		if (searchKey == null) {
+			searchKey = "subject";
+			searchValue = "";
+		}
+
+		searchValue = URLDecoder.decode(searchValue, "utf-8");
+
+		// 조회수 증가
+		dao.updateHitCount(num);
+
+		// 게시물 가져오기
+		studyBoardDTO dto = dao.readBoard(num);
+		if (dto == null) { // 게시물이 없으면 다시 리스트로
+			resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+			return;
+		}
+
+		dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+
+		// 이전글 다음글
+		studyBoardDTO preReadDto = dao.preReadBoard(dto.getNum(), searchKey, searchValue);
+		studyBoardDTO nextReadDto = dao.nextReadBoard(dto.getNum(), searchKey, searchValue);
+
+		// 리스트나 이전글/다음글에서 사용할 파라미터
+		String params = "page=" + page;
+		if (searchValue.length() != 0) {
+			params += "&searchKey=" + searchKey + "&searchValue=" + URLEncoder.encode(searchValue, "utf-8");
+		}
+
+		// JSP로 전달할 속성
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("params", params);
+		req.setAttribute("preReadDto", preReadDto);
+		req.setAttribute("nextReadDto", nextReadDto);
+
+		req.setAttribute("searchKey", searchKey);
+		req.setAttribute("searchValue", searchValue);
+
+		// 포워딩
+		String path = "/WEB-INF/views/studyBoard/article.jsp";
+		forward(req, resp, path);
+	}
+	
+	private void updateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 수정 폼
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		if (info == null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp + "/member/login.do");
+			return;
+		}
+
+		String page = req.getParameter("page");
+		int num = Integer.parseInt(req.getParameter("num"));
+		studyBoardDTO dto = dao.readBoard(num);
+
+		if (dto == null) {
+			resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+			return;
+		}
+
+		// 게시물을 올린 사용자가 아니면
+		if (!dto.getUserId().equals(info.getUserId())) {
+			resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+			return;
+		}
+
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("mode", "update");
+
+		String path = "/WEB-INF/views/studyBoard/created.jsp";
+		forward(req, resp, path);
+	}
+
+	private void updateSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 수정 완료
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		if (info == null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp + "/member/login.do");
+			return;
+		}
+
+		String page = req.getParameter("page");
+
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+			return;
+		}
+
+		studyBoardDTO dto = new studyBoardDTO();
+		dto.setNum(Integer.parseInt(req.getParameter("num")));
+		dto.setSubject(req.getParameter("subject"));
+		dto.setContent(req.getParameter("content"));
+
+		dao.updateBoard(dto);
+
+		resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 삭제
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		if (info == null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp + "/member/login.do");
+			return;
+		}
+
+		String page = req.getParameter("page");
+		int num = Integer.parseInt(req.getParameter("num"));
+		studyBoardDTO dto = dao.readBoard(num);
+
+		if (dto == null) {
+			resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+			return;
+		}
+
+		// 게시물을 올린 사용자나 admin이 아니면
+		if (!dto.getUserId().equals(info.getUserId()) && !info.getUserId().equals("admin")) {
+			resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+			return;
+		}
+
+		// studyBoardReply 테이블은 ON DELETE CASCADE 옵션으로 studyBoard 테이블의 데이터가 지워지면 자동 지워짐
+		dao.deleteBoard(num);
+		resp.sendRedirect(cp + "/studyBoard/list.do?page=" + page);
+	}
+
+	private void listReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 리플 리스트 ---------------------------------------
+		studyBoardDAO dao = new studyBoardDAO();
+		MyUtil util = new MyUtil();
+		
+		int num = Integer.parseInt(req.getParameter("num"));
+		String pageNo = req.getParameter("pageNo");
+		int current_page = 1;
+		if (pageNo != null)
+			current_page = Integer.parseInt(pageNo);
+
+		int numPerPage = 5;
+		int total_page = 0;
+		int dataCount = 0;
+
+		dataCount = dao.dataCountReply(num);
+		total_page = util.pageCount(numPerPage, dataCount);
+		if (current_page > total_page)
+			current_page = total_page;
+
+		int start = (current_page - 1) * numPerPage + 1;
+		int end = current_page * numPerPage;
+
+		// 리스트에 출력할 데이터
+		List<ReplyDTO> list = dao.listReply(num, start, end);
+
+		// 엔터를 <br>
+		Iterator<ReplyDTO> it = list.iterator();
+		while (it.hasNext()) {
+			ReplyDTO dto = it.next();
+			dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+		}
+
+		// 페이징처리(인수2개 짜리 js로 처리)
+		String paging = util.paging(current_page, total_page);
+
+		req.setAttribute("list", list);
+		req.setAttribute("pageNo", current_page);
+		req.setAttribute("dataCount", dataCount);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("paging", paging);
+
+		// 포워딩
+		String path = "/WEB-INF/views/studyBoard/listReply.jsp";
+		forward(req, resp, path);
+	}
+
+	private void insertReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 리플 저장하기 ---------------------------------------
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		String state="true";
+		if (info == null) { // 로그인되지 않은 경우
+			state="loginFail";
+		} else {
+			int num = Integer.parseInt(req.getParameter("num"));
+			ReplyDTO rdto = new ReplyDTO();
+			rdto.setNum(num);
+			rdto.setUserId(info.getUserId());
+			rdto.setContent(req.getParameter("content"));
+
+			int result=dao.insertReply(rdto);
+			if(result==0)
+				state="false";
+		}
+
+		StringBuffer sb=new StringBuffer();
+		sb.append("{");
+		sb.append("\"state\":"+"\""+state+"\"");
+		sb.append("}");
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out=resp.getWriter();
+		out.println(sb.toString());
+		
+	}
+
+	private void deleteReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 리플 삭제 ---------------------------------------
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		studyBoardDAO dao = new studyBoardDAO();
+		
+		int replyNum = Integer.parseInt(req.getParameter("replyNum"));
+		String userId=req.getParameter("userId");
+		
+		String state="false";
+		if (info == null) { // 로그인되지 않은 경우
+			state="loginFail";
+		} else if(info.getUserId().equals("admin") || info.getUserId().equals(userId)) {
+			dao.deleteReply(replyNum);
+			state="true";
+		}
+		
+		StringBuffer sb=new StringBuffer();
+		sb.append("{");
+		sb.append("\"state\":"+"\""+state+"\"");
+		sb.append("}");
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out=resp.getWriter();
+		out.println(sb.toString());
+	}
+}
